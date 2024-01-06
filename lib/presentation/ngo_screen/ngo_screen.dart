faf064591cb88e3d7a5089da1724faac4655f039
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2024-01-06 21:21:53 +0530

    first commit

--- lib/presentation/ngo_screen/ngo_screen.dart
+++ lib/presentation/ngo_screen/ngo_screen.dart
@@ -1,20 +1,107 @@
-import '../ngo_screen/widgets/userprofile_item_widget.dart';import 'package:flutter/material.dart';import 'package:meal_connect/core/app_export.dart';import 'package:meal_connect/presentation/ngo_order_list_page/ngo_order_list_page.dart';import 'package:meal_connect/widgets/app_bar/appbar_subtitle_two.dart';import 'package:meal_connect/widgets/app_bar/appbar_title.dart';import 'package:meal_connect/widgets/app_bar/appbar_trailing_iconbutton.dart';import 'package:meal_connect/widgets/app_bar/custom_app_bar.dart';import 'package:meal_connect/widgets/custom_bottom_bar.dart';
+import '../ngo_screen/widgets/userprofile_item_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:meal_connect/core/app_export.dart';
+import 'package:meal_connect/presentation/ngo_order_list_page/ngo_order_list_page.dart';
+import 'package:meal_connect/widgets/app_bar/appbar_subtitle_two.dart';
+import 'package:meal_connect/widgets/app_bar/appbar_title.dart';
+import 'package:meal_connect/widgets/app_bar/appbar_trailing_iconbutton.dart';
+import 'package:meal_connect/widgets/app_bar/custom_app_bar.dart';
+import 'package:meal_connect/widgets/custom_bottom_bar.dart';
+
 // ignore_for_file: must_be_immutable
-class NgoScreen extends StatelessWidget {NgoScreen({Key? key}) : super(key: key);
+class NgoScreen extends StatelessWidget {
+  NgoScreen({Key? key}) : super(key: key);
 
-GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
-@override Widget build(BuildContext context) { return SafeArea(child: Scaffold(appBar: _buildAppBar(context), body: SizedBox(width: double.maxFinite, child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [SizedBox(height: 49.v), _buildUserProfile(context)])), bottomNavigationBar: _buildBottomBar(context))); } 
-/// Section Widget
-PreferredSizeWidget _buildAppBar(BuildContext context) { return CustomAppBar(height: 94.v, title: Padding(padding: EdgeInsets.only(left: 38.h), child: Row(children: [AppbarSubtitleTwo(text: "MEAL\nCONNECT"), AppbarTitle(text: "NGO’s", margin: EdgeInsets.only(left: 39.h, top: 4.v))])), actions: [AppbarTrailingIconbutton(imagePath: ImageConstant.imgMoreVertical, margin: EdgeInsets.fromLTRB(38.h, 38.v, 38.h, 22.v))], styleType: Style.bgFill); } 
-/// Section Widget
-Widget _buildUserProfile(BuildContext context) { return Padding(padding: EdgeInsets.symmetric(horizontal: 29.h), child: ListView.separated(physics: NeverScrollableScrollPhysics(), shrinkWrap: true, separatorBuilder: (context, index) {return SizedBox(height: 26.v);}, itemCount: 5, itemBuilder: (context, index) {return UserprofileItemWidget(onTapUserProfile: () {onTapUserProfile(context);});})); } 
-/// Section Widget
-Widget _buildBottomBar(BuildContext context) { return CustomBottomBar(onChanged: (BottomBarEnum type) {Navigator.pushNamed(navigatorKey.currentContext!, getCurrentRoute(type));}); } 
-///Handling route based on bottom click actions
-String getCurrentRoute(BottomBarEnum type) { switch (type) {case BottomBarEnum.Explore: return AppRoutes.ngoOrderListPage; case BottomBarEnum.Ngo: return "/"; case BottomBarEnum.Notification: return "/"; case BottomBarEnum.Profile: return "/"; default: return "/";} } 
-///Handling page based on route
-Widget getCurrentPage(String currentRoute) { switch (currentRoute) {case AppRoutes.ngoOrderListPage: return NgoOrderListPage(); default: return DefaultWidget();} } 
-/// Navigates to the profileOtherScreen when the action is triggered.
-onTapUserProfile(BuildContext context) { Navigator.pushNamed(context, AppRoutes.profileOtherScreen); } 
- }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: _buildAppBar(context),
+            body: SizedBox(
+                width: double.maxFinite,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 49.v),
+                      _buildUserProfile(context)
+                    ])),
+            bottomNavigationBar: _buildBottomBar(context)));
+  }
+
+  /// Section Widget
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return CustomAppBar(
+        height: 94.v,
+        title: Padding(
+            padding: EdgeInsets.only(left: 38.h),
+            child: Row(children: [
+              AppbarSubtitleTwo(text: "MEAL\nCONNECT"),
+              AppbarTitle(
+                  text: "NGO’s", margin: EdgeInsets.only(left: 39.h, top: 4.v))
+            ])),
+        actions: [
+          AppbarTrailingIconbutton(
+              imagePath: ImageConstant.imgMoreVertical,
+              margin: EdgeInsets.fromLTRB(38.h, 38.v, 38.h, 22.v))
+        ],
+        styleType: Style.bgFill);
+  }
+
+  /// Section Widget
+  Widget _buildUserProfile(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 29.h),
+        child: ListView.separated(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 26.v);
+            },
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return UserprofileItemWidget(onTapUserProfile: () {
+                onTapUserProfile(context);
+              });
+            }));
+  }
+
+  /// Section Widget
+  Widget _buildBottomBar(BuildContext context) {
+    return CustomBottomBar(onChanged: (BottomBarEnum type) {
+      Navigator.pushNamed(navigatorKey.currentContext!, getCurrentRoute(type));
+    });
+  }
+
+  ///Handling route based on bottom click actions
+  String getCurrentRoute(BottomBarEnum type) {
+    switch (type) {
+      case BottomBarEnum.Explore:
+        return AppRoutes.ngoOrderListPage;
+      case BottomBarEnum.Ngo:
+        return "/";
+      case BottomBarEnum.Notification:
+        return "/";
+      case BottomBarEnum.Profile:
+        return "/";
+      default:
+        return "/";
+    }
+  }
+
+  ///Handling page based on route
+  Widget getCurrentPage(String currentRoute) {
+    switch (currentRoute) {
+      case AppRoutes.ngoOrderListPage:
+        return NgoOrderListPage();
+      default:
+        return DefaultWidget();
+    }
+  }
+
+  /// Navigates to the profileOtherScreen when the action is triggered.
+  onTapUserProfile(BuildContext context) {
+    Navigator.pushNamed(context, AppRoutes.profileOtherScreen);
+  }
+}
